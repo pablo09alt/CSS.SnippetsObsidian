77a49709bb4016e6e79729910e66856145f29799
PaCoalt <pschwarzwaelder@yahoo.com>
2026-01-21 21:50:54 +0100

    Suppression de l'ancien dossier mini_chrono/main.js

--- Plugins/mini_chrono/main.js
+++
@@ -1,9 +0,0 @@
-// Contenu du fichier main.js du mini_timer
-module.exports = {
-  onload: function() {
-    console.log('Mini Chrono plugin loaded');
-  },
-  onunload: function() {
-    console.log('Mini Chrono plugin unloaded');
-  }
-};
